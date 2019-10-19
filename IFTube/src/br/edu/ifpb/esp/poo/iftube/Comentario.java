b2304d9c06608a5d8c538a34f36164e37ec64a1b
Hugo Feitosa de Figeirêdo <hugoff@gmail.com>
2019-10-18 21:05:59 -0300

    Mudança de tipo das respostas.

--- IFTube/src/br/edu/ifpb/esp/poo/iftube/Comentario.java
+++ IFTube/src/br/edu/ifpb/esp/poo/iftube/Comentario.java
@@ -1,25 +1,26 @@
 package br.edu.ifpb.esp.poo.iftube;
 
 import java.util.Calendar;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Comentario {
 
 	private String texto;
 	private Calendar data;
 	private Usuario usuario;
-	private Map<Calendar, Comentario> respostas;
+	private Set<Comentario> respostas;
 	
 	public Comentario() {
-		respostas = new HashMap<Calendar, Comentario>();
+		respostas = new HashSet<Comentario>();
 	}
 	
-	public Map<Calendar, Comentario> getRespostas() {
+	public Set<Comentario> getRespostas() {
 		return respostas;
 	}
 	
-	public void setRespostas(Map<Calendar, Comentario> respostas) {
+	public void setRespostas(Set<Comentario> respostas) {
 		this.respostas = respostas;
 	}
 	
